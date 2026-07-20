b3ea2d1de3671787967a64e2a87450508e25c5d5
variantage-inc <support@variantage.com>
2026-07-19 22:43:57 -0400

    Organize project structure and move assets to public

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import Hero from "@/components/Hero";
+import Header from "@/components/layout/Header";
+import Hero from "@/components/sections/Hero";
 
 export default function Home() {
   return (
     <main>
+      <Header />
       <Hero />
     </main>
   );
